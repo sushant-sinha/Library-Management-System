eb394b71ee7636a83d7455e19d76b40de410dd98
sushant-sinha <sinhasushant04@gmail.com>
2021-11-05 22:11:16 +0530

    base

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2021 Library Management System |<a href="https://www.linkedin.com/in/super-sushant/" target="_blank"  > Designed by : Sushant Sinha</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
